5788c7b3778b318e78dced66bc976b084ea32bde
Annisa Azzahro <70183618+annisazhr@users.noreply.github.com>
2021-03-17 13:14:28 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman DInamis Dengan PHP</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		<h2>SMKN 7 BALEENDAH</h2> 
+		<p>Membuat Halaman Dinamis Dengan PHP</p>
+	</header>
+	<div id = "navbar">
+		<div class = "nav">
+			<a href="index.php?home">Home</a>
+			<a href="index.php?Article">Article</a>
+			<a href="index.php?About">About</a>
+			<a href="index.php?Contact">Contact</a>
+		</div>
+	</div>
+	<!--navbar-->
+	<div class="konten">
